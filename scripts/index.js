2be8738cada5151ba21955fb6e58027c6c188c86
Vladimir Pavlov <mandran@inbox.ru>
2022-03-17 21:49:57 +0800

    Завершить проект

---
+++ scripts/index.js
@@ -0,0 +1,50 @@
+// Открытие и закрытие Popup
+
+const popupElement = document.querySelector('.popup');
+const navButton = document.querySelector('.profile__name-button');
+const closeButton = popupElement.querySelector('.popup__close-button');
+
+function openPopup() {
+  popupElement.classList.add('popup_opened');
+  document.addEventListener('keyup', onDocumentKeyUp);
+  popupName.value = profileName.textContent;
+  popupProfession.value = profileProfession.textContent;
+}
+
+function closePopup() {
+  popupElement.classList.remove('popup_opened');
+  // Удаляем собития, которые мы передавали в addEventListener (с обработчика событий)
+  document.removeEventListener('keyup', onDocumentKeyUp);
+}
+
+function onDocumentKeyUp(event) {
+  if (event.key === ESC_KEY) {
+    closePopup();
+  }
+}
+
+navButton.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
+
+const ESC_KEY = "Escape";
+
+// Редактирование Popup
+
+let popupForm = document.querySelector('.popup__form');
+let popupName = popupForm.querySelector('.popup__name');
+let popupProfession = popupForm.querySelector('.popup__profession');
+
+let profileName = document.querySelector('.profile__name-text');
+let profileProfession = document.querySelector('.profile__profession');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = popupName.value;
+  profileProfession.textContent = popupProfession.value;
+
+  closePopup();
+}
+
+popupForm.addEventListener('submit', formSubmitHandler);
